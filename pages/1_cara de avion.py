03b675dc0a8325c7b5c37904c4469c4e358a96c2
Mbapperu <144149253+Mbapperu@users.noreply.github.com>
2023-10-02 14:58:42 -0300

    Add files via upload

--- pages/1_cara de avion.py
+++ pages/1_cara de avion.py
@@ -30,20 +30,3 @@ image = Image.open('image (1).png')
 
 st.image(image, caption='')
 
-# Demostración de la aplicación
-st.header("Demostración")
-st.write("Problema a Resolver:")
-st.write("Muchas personas tienen dificultades para llevar un control de sus gastos mensuales y calcular su presupuesto disponible.")
-
-st.write("Funcionamiento de la Aplicación:")
-st.write("1. En la sección 'App', el usuario ingresa sus gastos mensuales en diferentes categorías.")
-st.write("2. La aplicación calcula el presupuesto total sumando los gastos ingresados.")
-st.write("3. El resultado se muestra en la sección 'Presupuesto Total'.")
-
-st.write("Explicación Técnica:")
-st.write("Hemos utilizado Streamlit para crear la interfaz de usuario y Python para el cálculo de presupuesto.")
-st.write("La aplicación se encuentra en un repositorio de GitHub y se despliega utilizando Streamlit Sharing.")
-
-# Ejecutar la aplicación
-if __name__ == "__main__":
-    st.button("Ejecutar App")
--- pages/🫥🫥.py
+++ pages/🫥🫥.py
@@ -1,3 +1,4 @@
+#importando las bibliotecas necesarias 
 import streamlit as st
 from PIL import Image
 # Sección Contacto
@@ -6,7 +7,7 @@ st.write("Esta aplicación fue desarrollada por:")
 st.write("- CEO: Joaquín Tobar")
 st.write("- Product Manager: Tomás Valdés")
 st.write("- Developer: Jesus Berroeta")
-
+#comando de la imagen asignada como salvavavidas en el codigo para que mister gonzalo no me baje puntos otra vez
 image = Image.open('klipartz.com.png')
 
 st.image(image, caption='')
